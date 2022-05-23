97b57c8bddac31be9ce72538e2e053d96d98e806
Rian Irawan <73005569+Bladex7@users.noreply.github.com>
2022-05-23 21:25:47 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,156 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from db import create_table_news_0414
+import news_models
+
+
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+CORS(app)
+
+
+@app.route('/news', methods=['GET'])
+def get_news():
+    result = news_models.get_news()
+    
+    data = {
+            
+            'status': 200,
+            'data': result
+        
+        }
+    
+    resp = jsonify(data)
+    resp.status_code = 200
+    
+    return resp
+
+@app.route('/news/<news_id>', methods=['GET'])
+def get_news_by_news_id(news_id):
+    try:
+        result = news_models.get_news_by_news_id(news_id)
+        data = {
+                
+                'status': 200,
+                'data': result
+            
+            }
+        
+        resp = jsonify(data)
+        resp.status_code = 200
+        
+        return resp
+    except:
+        data = {
+                
+                'status': 404,
+                'message': "Data Not Found"
+            
+            }
+        
+        resp = jsonify(data)
+        resp.status_code = 404
+        
+        return resp
+        
+@app.route('/news', methods=['POST'])
+def insert_news():
+    
+    news_details = request.json
+    title = news_details['title']
+    content = news_details['content']
+    datetime = news_details['datetime']
+    flag = news_details['flag']
+    result = news_models.insert_news(title, content, datetime, flag)
+    
+    data = {
+        
+            'status': 201,
+            'message': 'Success!'
+        
+        }
+    
+    resp = jsonify(data)
+    resp.status_code = 201
+    
+    return resp
+
+@app.route('/news/<news_id>', methods=['PUT'])
+def update_students(news_id):
+    
+    news_details = request.json
+    news_id =  news_details['news_id']
+    title =  news_details['title']
+    content =  news_details['content']
+    datetime =  news_details['datetime']
+    flag =  news_details['flag']
+    result = news_models.update_news(news_id, title, content, datetime, flag)
+    
+    data = {
+        
+            'status': 200,
+            'message': 'Success!'
+        
+        }
+    
+    resp = jsonify(data)
+    resp.status_code = 200
+    
+    return resp
+
+@app.route('/news/<news_id>', methods=['PATCH'])
+def patch_students(news_id):
+    
+    news_details = request.json
+    news_id =  news_details['news_id']
+    flag =  news_details['flag']
+    result = news_models.patch_news(news_id, flag)
+    
+    data = {
+        
+            'status': 200,
+            'message': 'Success!'
+        
+        }
+    
+    resp = jsonify(data)
+    resp.status_code = 200
+    
+    return resp
+
+@app.route('/news/<news_id>', methods=['DELETE'])
+def delete_news(news_id):
+    result = news_models.delete_news(news_id)
+    
+    data = {
+            
+            'status': 200,
+            'message': "Success!"
+        
+        }
+    
+    resp = jsonify(data)
+    resp.status_code = 200
+    
+    return resp
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        
+            'status': 404,
+            'message': 'Not Found: ' + request.url
+        }
+    
+    resp = jsonify(message)
+    resp.status_code = 404
+    
+    return resp
+
+
+if __name__ == "__main__":
+    #create_table_students()
+    #print(get_data())
+    app.run(debug=True)
